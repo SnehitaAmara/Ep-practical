cc85e7aff3d1d150cf66f06be6b43170c232b3ca
SnehitaAmara <74001730+SnehitaAmara@users.noreply.github.com>
2021-01-21 23:27:03 +0530

    insertions class -- student management system

---
+++ studentInsertions.java
@@ -0,0 +1,54 @@
+package Insertions;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import Bean.studentBean;
+import Connections.DBUtil;
+
+
+public class studentInsertions {
+
+	public int studentInsert(studentBean student) throws ClassNotFoundException , SQLException {
+		Connection con =DBUtil.DBConnection();
+		//prepared-Statements
+		PreparedStatement ps = con.prepareStatement("insert into studentDB values(?,?,?)");
+		ps.setInt(1,student.getRegno());
+		ps.setString(2,student.getName());
+		ps.setString(3,student.getEmail());
+		int a=ps.executeUpdate();
+		con.close();
+		return a;
+	}
+	
+	public int studentDelete(studentBean student) throws ClassNotFoundException , SQLException {
+		Connection con =DBUtil.DBConnection();
+		PreparedStatement ps = con.prepareStatement("delete from studentDB where regno=?");
+		ps.setInt(1,student.getRegno());
+		int b=ps.executeUpdate();
+		con.close();
+		return b;
+	}
+	
+	public int studentUpdate(studentBean student) throws ClassNotFoundException , SQLException {
+		Connection con =DBUtil.DBConnection();
+		PreparedStatement ps = con.prepareStatement("update studentDB set name=? where regno=?");
+		ps.setInt(1,student.getRegno());
+		ps.setString(2,student.getName());
+		int c=ps.executeUpdate();
+		con.close();
+		return c;
+	}
+	
+	public void studentDisplay(studentBean student) throws ClassNotFoundException , SQLException {
+		Connection con =DBUtil.DBConnection();
+		PreparedStatement ps = con.prepareStatement("select * from studentDB where regno=?");
+		ResultSet rs = ps.executeQuery();
+		while(rs.next()) {
+			System.out.println(rs.getInt(1) + " "+rs.getString(2)+" "+rs.getString(3));
+		}
+	}
+	
+	
+}
