e91fdb97aa38ca60889e27f2c782e41e818ba632
SnehitaAmara <74001730+SnehitaAmara@users.noreply.github.com>
2021-01-21 23:28:17 +0530

    DButil class -- student management system

---
+++ DBUtil.java
@@ -0,0 +1,20 @@
+package Connections;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	final static String forNameURL="com.mysql.cj.jdbc.Driver";
+	final static String dbURL="jdbc:mysql://localhost:3306/ep_db";
+	final static String username="root";
+	final static String password="root";
+	
+	public static Connection DBConnection() throws SQLException, ClassNotFoundException {
+		Class.forName(forNameURL);
+		Connection con = DriverManager.getConnection(dbURL,username,password);
+		return con;
+	}
+	
+
+}
